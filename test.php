c1ba060972466b4c87d9125ca9c13c87d0b004bf
Itfee <ainet@126.com>
2023-04-28 10:58:01 +0800

    First

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+require './vendor/autoload.php';
+
+use royfee\feepdf\Fpdi;
+
+$fpdi = new Fpdi();
+$fpdi->AddGBFont('simhei', '黑体');
+
+/*
+$fpdi->setSourceFile('1.pdf');
+$tplid = $fpdi->importPage(1);
+$specs = $fpdi->getTemplateSize($tplid);
+$fpdi->AddPage('P',[$specs['width'],$specs['height']]);
+$fpdi->useTemplate($tplid);
+*/
+
+$fpdi->AddPage('P',[100,150]);
+
+//面单添加文字
+$fpdi->SetFont('simhei','',13);
+$fpdi->SetXY(1, 10);
+$fpdi->Write(0,iconv("UTF-8","gbk",'中文测试'));
+
+$fpdi->SetXY(1, 129);
+$fpdi->Write(0,iconv("UTF-8","gbk",'打印测试'));
+
+$fpdi->Output();
